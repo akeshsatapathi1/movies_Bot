f40fe1208ee45bb61906978d169e52b2e27c984d
SK- KING <gan49ylf@yahoo.com>
2024-11-01 14:26:02 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/SAKSHI_SUPPORT'
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002264239316').split()]
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://lalecim863:AZCkLNq5BBIsEtfs@cluster0.zvwff.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://lalecim863:FAZIXsVFezX5XXCq@skking.rkmru.mongodb.net/?retryWrites=true&w=majority&appName=Skking")
 DATABASE_NAME = environ.get('DATABASE_NAME', "lalecim863")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 #---------------------------------------------------------------
